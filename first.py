831cae38f2f859906876a6c9cda3832517fea51e
Kashafkkk <kashafeman304@gmail.com>
2024-10-30 05:49:06 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,35 @@
+number1=4
+number2=8
+number3=7
+#calculate sum 
+sum=number1+number2+number3
+print("sum:",sum)
+#calculated diffrence
+diffrence=number1-number2-number3
+print("diffrence:",diffrence)
+#calculated product
+product=number1*number2*number3
+print("product:",product)
+#calculated average
+average=(number1+number2+number3)/3
+print("average",average)
+
+#area of triangLe
+breadth=29
+height=16
+area=0.5*breadth*height
+print("triangle area",area )
+#area of suqare 
+side=11
+area=side*side
+print("square area",area )
+#area of circle 
+pi=3.14
+radius=9
+area=pi*radius*radius 
+print("circle area",area )
+#rectangle area 
+length=19
+width=21
+area=length*width
+print("rectangle area",area)
